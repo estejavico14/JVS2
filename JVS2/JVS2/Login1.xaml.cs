422e1afae2589d9eaafe9b47753a93bd87f6b6d1
estej <estej@CASA-PC>
2023-01-03 16:48:51 -0500

    Agregar archivos de proyecto.

---
+++ JVS2/JVS2/Login1.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JVS2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "estudiante2022";
+            string contraseña = "uisrael2022";
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushModalAsync (new Login());
+
+            }
+            else
+            {
+                DisplayAlert("ALERTA", "Usuario/Contraseña Incorrectos", "Cerrar");
+            }
+        }
+    }
+}
